660d1b589a5a9b3bafb3bee820c34d5c1ddf5bc3
Paddy Dong <paddy.dong@activenetwork.com>
2018-01-23 13:46:40 +0800

    add html test report

--- api_test_assistant_main.py
+++ api_test_assistant_main.py
@@ -1,4 +1,8 @@
 import unittest
+from pyunitreport import HTMLTestRunner
+import parse_api_test_cases
+import os
+import time
 
 class ApiTestCase(unittest.TestCase):
     def __init__(self, testcase_dict):
@@ -20,12 +24,27 @@ testcase_dict02 = {
     'url': "https://int-vebweb-vip.dev.activenetwork.com/api/internal/campaigns?agency_id=8809",
     'status_code': 200
 }
+test_suite_name = 'demo'
 
 if __name__ == '__main__':
     suite = unittest.TestSuite()
+    ApiTestCase.runTest.__doc__ = 'testcase_dict01'
     test = ApiTestCase(testcase_dict01)
+
     suite.addTest(test)
+
+    ApiTestCase.runTest.__doc__ = 'testcase_dict02'
     test = ApiTestCase(testcase_dict02)
+
     suite.addTest(test)
-    runner = unittest.TextTestRunner()
+    test_report_folder_path = os.path.join(os.getcwd(), 'tests/reports/')
+    if not os.path.exists(test_report_folder_path):
+        os.makedirs(test_report_folder_path)
+    time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+    test_report_file_path = str(test_suite_name) + time
+    kwargs = {
+        "output": test_report_folder_path,
+        "report_name": test_report_file_path
+    }
+    runner = HTMLTestRunner(**kwargs)
     runner.run(suite)
--- parse_api_test_cases.py
+++ parse_api_test_cases.py
@@ -1,7 +1,24 @@
 import yaml
 import os
+import io
+
+class TestCaseParser:
+
+    def __init__(self, test_suite_name):
+        self.test_suite_name = test_suite_name
+        self.file_path = ""
+
+    def get_file_path_by_test_suite_name(self):
+        self.file_path = os.path.join(os.getcwd(), 'tests/test_cases_list/' + str(self.test_suite_name) + '.yml')
+        return  self.file_path
+
+    def get_test_case_list(self):
+        self.get_file_path_by_test_suite_name()
+        with io.open(self.file_path, 'r', encoding='utf-8') as stream:
+            test_case_list = yaml.load(stream)
+            return test_case_list
 
 
-def get_file
 
-file_path = os.path.join(os.getcwd(), 'tests/test_cases_list/demo_testset_hardcode.yml')
+tcp = TestCaseParser('demo_test_suite')
+tcp.get_test_case_list()
